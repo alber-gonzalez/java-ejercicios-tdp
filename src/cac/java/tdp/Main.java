a2f7e954b0322c1fbaf1303beeab315b2663626d
alber-gonzalez <albertonicolas.gonzalez@gmail.com>
2017-08-15 20:05:58 -0300

    Ejercicio que declara variable int y realiza operaciones

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -17,6 +17,14 @@ public class Main {
      */
     public static void main(String[] args) {
         
+        int N;
+        
+        N = 3;
+        
+        System.out.println(N+77);
+        System.out.println(N-3);
+        System.out.println(N*2);
+        
     }
     
 }
