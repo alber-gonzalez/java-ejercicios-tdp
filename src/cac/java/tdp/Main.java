257608b72bc0465643d8e9abf268df0b09c53b39
alber-gonzalez <albertonicolas.gonzalez@gmail.com>
2017-08-19 21:14:16 -0300

    Este es un ejercicio que recibe un número por teclado y muestra por pantalla el doble y el triple del mismo

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -3,6 +3,7 @@
  * Lee un número entero por teclado y obtiene y muestra por pantalla el doble y el triple de ese número.
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
@@ -14,6 +15,15 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner (System.in);
+        
+        System.out.print("Ingresa un número entero: ");
+        int numero = teclado.nextInt();
+        System.out.print("El doble del número elegido es: ");
+        System.out.println(numero*2);
+        System.out.print("El triple del número elegido es: ");
+        System.out.println(numero*3);
         
     }
     
