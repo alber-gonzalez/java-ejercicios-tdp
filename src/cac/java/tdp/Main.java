9bb8fec45fed7e635c3872a286971275944d2e2b
alber-gonzalez <albertonicolas.gonzalez@gmail.com>
2017-09-05 23:12:22 -0300

    Programa que lee una temperatura en grados centígrados y los tranforma a grados fahrenheit.

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -4,6 +4,7 @@
  * La fórmula correspondiente es: F = 32 + ( 9 * C / 5)
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +16,12 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresá la temperatura en grados centígrados");
+        double temperatura = teclado.nextDouble();
+        System.out.println("La temperatura en grados Faharenheit es de: " + (32 + ((9*temperatura)/5)));
+        
         
     }
     
