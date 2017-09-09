0967588cc288946e964c0e654f096e5fa1cf2111
alber-gonzalez <albertonicolas.gonzalez@gmail.com>
2017-09-09 19:30:55 -0300

    Este es un ejercicio que recibe por teclado el radio de una circunsferencia, y calcula su llongitud y área.

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -5,6 +5,7 @@
  * circunferencia = 2*PI*Radio, Area de la circunferencia = PI*Radio^2  
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +17,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+     
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Ingresá el radio de una circunsferencia: ");
+        double radio = teclado.nextDouble();
+        double pi = 3.14;
+         System.out.println("La longitud de la circunsferencia es: "+(2*pi*radio));
+         System.out.println("El área de la circunsferencia es: "+(pi*(radio*radio)));
+         
+        
         
     }
     
