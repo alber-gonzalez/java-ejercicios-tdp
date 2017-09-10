0ffd7bca202d62861c79e6ef770d516d52a3b1d4
alber-gonzalez <albertonicolas.gonzalez@gmail.com>
2017-09-09 22:10:05 -0300

    Este es un ejercicio que tranforma velocidades expresadas en km por hora a mt por segundo

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -3,6 +3,7 @@
  * La velocidad se lee por teclado.
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
@@ -14,6 +15,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in); 
+        System.out.println("Ingrese la velocidad expresada en km. por hora");
+        double velocidad = teclado.nextDouble();
+        System.out.println("La velocidad es de: "+ ((velocidad*1000)/3600)+"m/s.");
         
     }
     
