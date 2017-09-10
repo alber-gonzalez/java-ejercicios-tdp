774b2973791dc39719090a2b0820949caeec9f5b
alber-gonzalez <albertonicolas.gonzalez@gmail.com>
2017-09-10 11:51:23 -0300

    Ejercicio que recibe los catetos de un triángulo y calcula la hipotenusa del mismo

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -4,18 +4,27 @@
  * teorema de Pitágoras. 
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
  * @author Rhaynel Parra Aguiar <mail@rhynl.io>
  */
 public class Main {
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
+        Scanner teclado = new Scanner (System.in);
+        double catetoMayor, catetoMenor;
+        System.out.println("Ingrese la longitud del cateto mayor: ");
+        catetoMayor = teclado.nextDouble();
+        System.out.println("Ingrese la longitud del cateto menor: ");
+        catetoMenor = teclado.nextDouble();
+        System.out.println("La hipotenusa es: "+Math.sqrt(Math.pow(catetoMayor,2)
+        +Math.pow(catetoMenor,2)));
     }
-    
 }
