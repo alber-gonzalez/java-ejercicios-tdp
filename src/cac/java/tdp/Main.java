548f2a6273355a08a78311b545a24d97d6021d5f
Alberto <albertonicolas.gonzalez@gmail.com>
2017-09-10 22:42:45 -0300

    Ejercicio que recibe el radio de una esfera y calcula el volumen de la misma.

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -2,6 +2,7 @@
  * Ejercicio 15: Programa que calcula el volumen de una esfera.
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
@@ -14,6 +15,10 @@ public class Main {
      */
     public static void main(String[] args) {
         
+    Scanner teclado = new Scanner(System.in);
+    double radio, volumen;
+    System.out.println("Ingrese el radio de una esfera:");
+    radio = teclado.nextDouble();
+    System.out.println("El volumen de la esfera es "+(volumen=(4.0/3)*Math.PI*Math.pow(radio,3)));    
     }
-    
 }
